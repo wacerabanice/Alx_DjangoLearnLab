596067328a763631ffad615119c7a1c4b699d25f
Winnie Banice <93024882+wacerabanice@users.noreply.github.com>
2025-11-09 12:04:45 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book
 from .models import Library
+from django.views.generic.detail import DetailView
 
 # Function-based view: list all books
 def list_books(request):
