5305ec61f06c1a079b463ac561572ce7fcdf7b3a
Wacera <winniebanice@gmail.com>
2025-11-27 22:18:43 +0300

    update

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import BookList, BookViewSet
 
 router = DefaultRouter()
@@ -8,4 +9,7 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
+
+    # Token endpoint
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,16 +1,13 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, permissions
 from .models import Book
 from .serializers import BookSerializer
-
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users
 
-# Full CRUD ViewSet
 class BookViewSet(viewsets.ModelViewSet):
-    """
-    Provides list, retrieve, create, update, and delete actions for Book model.
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users
