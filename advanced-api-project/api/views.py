d523ee2a46cd3a5f0d57366d3b6a7b61cfb684b4
Wacera <winniebanice@gmail.com>
2025-11-30 18:19:57 +0300

    update

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework as filters
 
 from .models import Book
 from .serializers import BookSerializer
