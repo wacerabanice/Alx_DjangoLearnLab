60a8caa1cf74a497d2668b945d14b0c68006770b
Wacera <winniebanice@gmail.com>
2025-11-30 17:40:44 +0300

    update

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    # Stores the author's name
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    # Title of the book
+    title = models.CharField(max_length=200)
+
+    # Year the book was published
+    publication_year = models.IntegerField()
+
+    # Each book belongs to one author
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
